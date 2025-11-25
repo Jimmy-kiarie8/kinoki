c33b410540434818fbd8f23d6f195c4315558be3
Jimmy <jimkiarie8@gmail.com>
2025-11-25 09:17:01 +0300

    Sheet uploads

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5XPKGNVH');
+            })(window,document,'script','dataLayer','GTM-WSBMR2QQ');
           `}
         </Script>
         {/* End Google Tag Manager */}
@@ -86,7 +86,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5XPKGNVH"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WSBMR2QQ"
             height="0" 
             width="0" 
             style={{display: 'none', visibility: 'hidden'}}
